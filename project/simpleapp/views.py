4d42185bf5f77f1eea5a3b7d0dac8419a16a23f0
Christopher <den.platov.1999@mail.ru>
2022-03-11 16:28:44 +0600

    'the_first_web_project'

---
+++ project/simpleapp/views.py
@@ -0,0 +1,24 @@
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView  # импортируем класс получения деталей объекта
+from .models import Product
+from datetime import datetime
+
+
+class ProductList(ListView):
+    model = Product
+    template_name = 'products.html'
+    context_object_name = 'products'
+    queryset = Product.objects.order_by('-id')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context[
+            'value1'] = None
+        return context
+
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product'
