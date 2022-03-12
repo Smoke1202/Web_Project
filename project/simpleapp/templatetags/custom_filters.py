83bfbbd880485217f30029c583f43e4b399cef3c
Christopher <den.platov.1999@mail.ru>
2022-03-12 17:10:25 +0600

    the_web_project_

--- project/simpleapp/templatetags/custom_filters.py
+++ project/simpleapp/templatetags/custom_filters.py
@@ -3,9 +3,17 @@ from django import template
 register = template.Library()
 
 
-@register.filter(name='multiply')
-def multiply(value, arg):
-    if isinstance(value, str) and isinstance(arg, int):
-        return str(value) * arg
-    else:
-        raise ValueError(f'Нельзя умножить {type(value)} на {type(arg)}')
+STRONG_WORDS = ["Аниме", "сериал", "News"]
+
+
+@register.filter(name='filtercen')
+def censor(value):
+   if not isinstance(value, str):
+       raise ValueError('Нельзя цензурировать не строку')
+
+   for word in STRONG_WORDS:
+       value = value.replace(word[1:], '*' * (len(word)-1))
+
+   return value
+
+
--- project/simpleapp/views.py
+++ project/simpleapp/views.py
@@ -6,19 +6,18 @@ from datetime import datetime
 
 class ProductList(ListView):
     model = Product
-    template_name = 'products.html'
-    context_object_name = 'products'
+    template_name = 'news.html'
+    context_object_name = 'news'
     queryset = Product.objects.order_by('-id')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['time_now'] = datetime.utcnow()
-        context[
-            'value1'] = None
+        context['value1'] = None
         return context
 
 
 class ProductDetail(DetailView):
     model = Product
-    template_name = 'product.html'
-    context_object_name = 'product'
+    template_name = 'new.html'
+    context_object_name = 'new'
